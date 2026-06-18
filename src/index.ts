df3616f805adf8457ad4055ce2eb130a35063aa8
Mikhail Zuenko <zuenko.michail@gmail.com>
2026-06-18 13:31:41 +0700

    Add export parse submodule

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './create/index.ts';
 export * from './element.ts';
 export * from './load.ts';
 export * from './node.ts';
+export * from './parse/index.ts';
 export * from './query.ts';
 export * from './xpath.ts';
